fc8089bb7b307ef1f0e07a53865cf93ada56516f
duyvu <vuthanhduy93@gmail.com>
2021-09-09 21:48:39 +0700

    fix count

--- public/counter.js
+++ public/counter.js
@@ -1,5 +1,26 @@
-let count = 0
-const value = document.getElementById("value")
-var btns = document.querySelectorAll(".btn")
-value.innerHTML = 2
-console.log(value)
+document.addEventListener('DOMContentLoaded', function () {
+    let count = 0;
+    const value = document.querySelector('#value')
+    const btns = document.querySelectorAll('.btn')
+    btns.forEach(function (item){
+        item.addEventListener('click',function(e){
+            const style =   e.currentTarget.classList
+            if(style [1] === 'increase'){
+                count++;
+            } else if(style [1]=== 'decrease'){
+                count--;
+            } else {
+                count = 0;
+            }
+            if(count > 0){
+                value.style.color = 'green'
+            } else if (count < 0){
+                value.style.color = 'red';
+            }else {
+                value.style.color = 'black'
+            }
+            value.innerHTML = count
+
+        })
+    })
+})
