cf9704ebc36d9ada8f01d8472fb799afd2dd6b5a
duyvu <vuthanhduy93@gmail.com>
2021-09-09 18:39:43 +0700

    some thing

--- public/counter.js
+++ public/counter.js
@@ -1,19 +1,5 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const counter = document.getElementById('counter')
-    const reset = document.getElementById('reset')
-    const decrease = document.getElementById('btn-decrease')
-    const increase = document.getElementById('btn-increase')
-    let x = 0
-    let x1 =0
-    decrease.onclick = function(){
-        x-=1
-        counter.innerHTML = x
-    }
-    increase.onclick= function(){
-        x+=1
-        counter.innerHTML = x;
-    }
-    reset.onclick=function(){
-        counter.innerHTML =x1;
-    }
-})
+let count = 0
+const value = document.getElementById("value")
+var btns = document.querySelectorAll(".btn")
+value.innerHTML = 2
+console.log(value)
