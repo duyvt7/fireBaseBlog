66ba54c012879d45d0046746ab46754f37b2196b
duyvt <vuthanhduy93@gmail.com>
2021-09-28 19:23:01 +0700

    map personnalize

--- public/projects/strava/strava.js
+++ public/projects/strava/strava.js
@@ -10,15 +10,19 @@ function getActs(res){
     .then((res)=>
         res.json()
     )
-    .then((data) => {
-        displayActs(data)
-    })
+    // .then((data) => {
+    //     displayActs(data)
+    // })
     .then(function(data){
         var map = L.map('map').setView([51.505, -0.09], 13);
 
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
+
+        for (var x=0; x<data.length;x++){
+            console.log(data[x].map.summary_polyline)
+        }
     })
 }
 function displayActs(data){
