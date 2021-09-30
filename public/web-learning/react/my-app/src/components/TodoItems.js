9dfbff591d744888ae0fdd994d3e905ac5ec0275
duyvu <vuthanhduy93@gmail.com>
2021-09-30 16:38:52 +0700

    create react app

---
+++ public/web-learning/react/my-app/src/components/TodoItems.js
@@ -0,0 +1,14 @@
+import React, { Component} from 'react';
+
+class TodoItem extends Component{
+    render() {
+        return (
+            <div className='todoItem'>
+                <p>đi chợ</p>
+                <p>đi chợ</p>
+                <p>đi chợ</p>
+            </div>
+        )
+    }
+}
+export default TodoItem;
