bd38421f76d35e51a9cd8c5ae1f83d5917e3a798
duyvt <vuthanhduy93@gmail.com>
2021-10-04 19:05:33 +0700

    finnish trafic light

--- public/web-learning/react/trafic-light/src/component/traficlight.js
+++ public/web-learning/react/trafic-light/src/component/traficlight.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './traficlight.css'
+import classNames from 'classnames'
 
 const RED = 0;
 const YELLOW = 1;
@@ -8,7 +9,15 @@ const  GREEN = 2;
 class TraficLight extends Component{
     constructor(){
         super();
-        this.currentColor = RED;
+        this.state = {
+            currentColor: GREEN
+        };
+        setInterval(()=>{
+            
+            this.setState({
+                currentColor: this.getNextColor(this.state.currentColor)
+            })
+        },1000)
     }
     getNextColor(color){
         switch(color){
@@ -22,11 +31,18 @@ class TraficLight extends Component{
         }
     }
     render(){
+        const {currentColor} = this.state
         return(
             <div className='TraficLight'>
-                <div className='buld red' />
-                <div className='buld yellow' />
-                <div className='buld green' />
+                <div className= {classNames('buld', 'red',{
+                    active: currentColor === RED
+                })} />
+                <div className= {classNames('buld', 'yellow',{
+                    active: currentColor === YELLOW
+                })} />
+                <div className= {classNames('buld', 'green',{
+                    active: currentColor === GREEN
+                })} />
             </div>
         )
     }
